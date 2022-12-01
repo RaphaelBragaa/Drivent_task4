a9d0311bafbd53f78a537b61a0c8a98ecf9ed29b
Raphael Braga <rbragamouraa@gmail.com>
2022-11-30 22:10:41 -0300

    feat: create bookingService

--- src/services/booking-service/index.ts
+++ src/services/booking-service/index.ts
@@ -1,4 +1,5 @@
 import BookingRepository from "@/repositories/booking-repository";
+import { listHotels } from "../hotels-service";
 import { notFoundError, requestedResourceForbiddenError } from "@/errors";
 
 async function getBooking(userId: number) {
@@ -25,13 +26,31 @@ async function postBooking(roomId: number, userId: number) {
   await BookingRepository.InsertBooking(booking);
 }
 
-//async function putBooking(roomId: number) {
-//}
+async function putBooking(roomId: number, userId: number) {
+  await listHotels(userId);
+  const result = BookingRepository.FindBookingById(userId);
+  if(!result) {
+    throw notFoundError;
+  }
+  const room = BookingRepository.FindRoomById(roomId);
+  if(!room) {
+    throw notFoundError;
+  }
+  if((await room).capacity === (await room).Booking.length) {
+    throw requestedResourceForbiddenError;
+  }
+  const booking = {
+    userId,
+    roomId
+  };
+  await BookingRepository.DeleteBooking((await result).id);
+  await BookingRepository.InsertBooking(booking);
+}
 
 const bookingService = {
   getBooking,
   postBooking,
-  //putBooking
+  putBooking
 };
 
 export default bookingService;
